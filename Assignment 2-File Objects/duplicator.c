4a39da58ef315e0a2026d38e93f0d97964b1a860
KhaNguyen04 <88961521+KhaNguyen04@users.noreply.github.com>
2022-05-29 01:21:07 -0700

    Grade:100/100
    Grade:100/100

---
+++ Assignment 2-File Objects/duplicator.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(int argc,char **argv) {
+    if (argc==1 || argc>2){
+        printf("USAGE: duplicator count\ncount must be an integer greater than 0\n");
+        exit(1);
+    }
+    else {
+        char *line = NULL;
+        char *ptr = NULL;
+        int count = strtol(argv[1], &ptr, 10);
+        if (count <= 0 || *ptr!='\0') {
+            printf("USAGE: duplicator count\ncount must be an integer greater than 0\n");
+            free(line);
+            exit(1);
+        }
+        else {
+            size_t len = 0;
+            while (getline(&line, &len, stdin)!=-1) {
+                line[strlen(line) - 1] = '\0';
+                if (count <= 100) {
+                    for (int i = 0; i < count; i++) {
+                        printf("%s\n", line);
+                    }
+                } else {
+                    printf("%s %d times\n", line, count);
+                }
+            }
+        }
+        free(line);
+        exit(0);
+    }
+}
